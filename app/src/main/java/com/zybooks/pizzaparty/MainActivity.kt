0e59d76f54c817771be531fe3edc2ffd5025680a
Peter Ryan <147430628+peterryan12@users.noreply.github.com>
2025-01-21 18:24:11 -0600

    Add number input and radio buttons

--- app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
+++ app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
@@ -4,19 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -82,6 +90,18 @@ fun NumberField(
     modifier: Modifier = Modifier
 ) {
     // Not implemented yet
+    var textInput by remember { mutableStateOf("") }
+
+    TextField(
+        value = textInput,
+        onValueChange = { textInput = it },
+        label = { Text(labelText) },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Number
+        ),
+        modifier = modifier
+    )
 }
 
 @Composable
@@ -92,6 +112,33 @@ fun RadioGroup(
     modifier: Modifier = Modifier
 ) {
     // Not implemented yet
+    var selectedOption by remember { mutableStateOf(selectedValue) }
+    val isSelectedOption: (String) -> Boolean = { selectedOption == it }
+
+    Column {
+        Text(labelText)
+        radioOptions.forEach { option ->
+            Row(
+                modifier = modifier
+                    .selectable(
+                        selected = isSelectedOption(option),
+                        onClick = { selectedOption = option },
+                        role = Role.RadioButton
+                    )
+                    .padding(8.dp)
+            ) {
+                RadioButton(
+                    selected = isSelectedOption(option),
+                    onClick = null,
+                    modifier = modifier.padding(end = 8.dp)
+                )
+                Text(
+                    text = option,
+                    modifier = modifier.fillMaxWidth()
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
